c4243037ce11cc353bb04997d87acd8093118436
claudio-py <claudio.3777@gmail.com>
2024-08-07 00:17:06 -0300

    added pages

---
+++ src/pages/Serviceum.js
@@ -0,0 +1,21 @@
+import Button from "../components/Button"
+export default function Serviceum (){
+  return(
+<section >
+      <div className="infobox">
+        <h2>Olhar atencioso e individualizado</h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas laboriosam fugiat, amet ab debitis commodi
+          sequi officiis labore nemo, nisi maxime alias! Molestiae consequuntur voluptatum eveniet suscipit in ea odio!
+        </p> 
+        <Button text='Descubra o tratamento ideal para você'url='#'/>
+      </div>
+      <div className="service-buttons">
+        <Button text='Descubra o tratamento ideal para você' url='#' />
+        <Button text='Descubra o tratamento ideal para você' url='#' />
+        <Button text='Descubra o tratamento ideal para você' url='#' />
+        <Button text='Descubra o tratamento ideal para você' url='#' />
+
+      </div>
+</section>
+  )
+}
