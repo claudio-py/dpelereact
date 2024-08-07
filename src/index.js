60d3e7185f4dac9e3a93b5eec745b393c4b6e735
claudio-py <claudio.3777@gmail.com>
2024-08-07 20:06:53 -0300

    some fixes

--- src/index.js
+++ src/index.js
@@ -5,16 +5,14 @@ import App from './App'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Wpp from './components/Wpp'
-import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <App  />
+    <Wpp />
     <Footer/>
-    <Wpp/>
   </React.StrictMode>
 );
-serviceWorker.register();
 
